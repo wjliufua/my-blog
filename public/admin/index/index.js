74fcc1f7fe9c15c77ac538bb7690a6064af4d2a5
wdnmd <1459200389@qq.com>
2020-11-02 17:52:18 +0800

    侧边栏动画效果完善

--- public/admin/index/index.js
+++ public/admin/index/index.js
@@ -1,5 +1,11 @@
+// 侧边栏选项
 var sideli = document.getElementsByClassName('sideli');
+// 侧边栏选项居中内容
 var sidelicenter = document.getElementsByClassName('sidelicenter');
+// 侧边栏选项展开内容
+var sidedl = document.getElementsByClassName('sidedl');
+sidedl[0].children[0].style.backgroundColor = '#808080';
+sidedl[0].children[0].children[0].style.color = '#fff';
 
 // 侧边栏选中效果
 function sideliAddress() {
@@ -19,20 +25,56 @@ function sideliAddress() {
     }
 }
 sideliAddress();
-var pull = document.getElementsByClassName('side_pull');
-var pullArr = [];
-// 控制小三角下拉，上拉
-function triangle() {
-    for (let i = 0; i < pull.length; i++) {
-        pullArr.push({ sideMenu: false });
-        pull[i].onclick = function() {
-            pullArr[i].sideMenu = !pullArr[i].sideMenu;
-            if (pullArr[i].sideMenu) {
-                $(`.side_pull:nth(${i})`).css('border-color', 'transparent transparent #fff');
-            } else {
-                $(`.side_pull:nth(${i})`).css('border-color', '#fff transparent transparent');
+// 侧边栏点击下拉效果
+var sideliArr = [];
+var sideflag = false;
+
+function sideClick() {
+    setTimeout(function() { sideli[0].click() }, 0);
+    for (let x = 0; x < sidedl[0].children[0].length; x++) {
+        sidedl[0].children[x].onclick = function(event) {
+            console.log(event);
+            event.stopPropagation();
+            sidedl[0].children[x].style.backgroundColor = '#808080';
+            sidedl[0].children[x].children[0].style.color = '#fff';
+        }
+    }
+    for (let i = 0; i < sideli.length; i++) {
+        sideliArr.push({ pull: false });
+        sideli[i].onclick = function() {
+            var sidelicolor = sideli[i].children[0];
+            for (let j = 0; j < sideli.length; j++) {
+                if (i != j) {
+                    sideliArr[j].pull = false;
+                }
+                sideli[j].children[0].children[0].style.color = '#D2D2D2';
+                sideli[j].children[0].children[1].style.color = '#D2D2D2';
+                if (sideli[j].children[0].children[2] == undefined) {
+                    continue
+                }
+                sideli[j].children[0].children[2].style.borderColor = '#D2D2D2 transparent transparent';
+                sideli[j].children[1].style.display = 'none';
             }
+            sideliArr[i].pull = !sideliArr[i].pull;
+            // sideflag = !sideflag;
+            // console.log(sideflag);
+            if (sideliArr[i].pull) {
+                sidelicolor.children[0].style.color = '#fff';
+                sidelicolor.children[1].style.color = '#fff';
+                if (sideli[i].children[0].children[2] !== undefined) {
+                    sideli[i].children[0].children[2].style.borderColor = 'transparent transparent #fff';
+                    sideli[i].children[1].style.display = 'block';
+                }
+            } else {
+                sidelicolor.children[0].style.color = '#D2D2D2';
+                sidelicolor.children[1].style.color = '#D2D2D2';
+                if (sideli[i].children[0].children[2] !== undefined) {
+                    sideli[i].children[0].children[2].style.borderColor = '#fff transparent transparent';
+                    sideli[i].children[1].style.display = 'none';
+                }
+            }
+            // console.log(sideliArr);
         }
     }
 }
-triangle();
+sideClick();
