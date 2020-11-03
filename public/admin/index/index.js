dab7be87ec537cf59ef671dea79e14c548c1b1e0
wdnmd <1459200389@qq.com>
2020-11-03 17:49:26 +0800

    侧边栏基本完成

--- public/admin/index/index.js
+++ public/admin/index/index.js
@@ -2,10 +2,12 @@
 var sideli = document.getElementsByClassName('sideli');
 // 侧边栏选项居中内容
 var sidelicenter = document.getElementsByClassName('sidelicenter');
+// 侧边栏
+var side = document.getElementsByClassName('side');
 // 侧边栏选项展开内容
 var sidedl = document.getElementsByClassName('sidedl');
-sidedl[0].children[0].style.backgroundColor = '#808080';
-sidedl[0].children[0].children[0].style.color = '#fff';
+var sideOpen = true;
+var sideOpens = true;
 
 // 侧边栏选中效果
 function sideliAddress() {
@@ -24,57 +26,244 @@ function sideliAddress() {
         }
     }
 }
-sideliAddress();
-// 侧边栏点击下拉效果
-var sideliArr = [];
-var sideflag = false;
 
-function sideClick() {
-    setTimeout(function() { sideli[0].click() }, 0);
-    for (let x = 0; x < sidedl[0].children[0].length; x++) {
-        sidedl[0].children[x].onclick = function(event) {
-            console.log(event);
-            event.stopPropagation();
-            sidedl[0].children[x].style.backgroundColor = '#808080';
-            sidedl[0].children[x].children[0].style.color = '#fff';
-        }
-    }
-    for (let i = 0; i < sideli.length; i++) {
-        sideliArr.push({ pull: false });
-        sideli[i].onclick = function() {
-            var sidelicolor = sideli[i].children[0];
-            for (let j = 0; j < sideli.length; j++) {
-                if (i != j) {
-                    sideliArr[j].pull = false;
+sideliAddress();
+var sidelicenter = document.getElementsByClassName('sidelicenter');
+var sideArr = [];
+var pull = true;
+
+// 点击下拉效果
+function sidelipull() {
+    setTimeout(function() { sidelicenter[0].click() }, 0);
+    for (let i = 0; i < sidelicenter.length; i++) {
+        sideArr.push({ pull: false });
+        sidelicenter[i].onclick = function() {
+            // for (let j = 0; j < sidelicenter.length; j++) {
+            //     if (i != j) {
+            //         sideArr[j].pull = false;
+            //     }
+            //     sidelicenter[j].children[0].style.color = '#D2D2D2';
+            //     sidelicenter[j].children[1].style.color = '#D2D2D2';
+            //     if (sideli[j].children[0].children[2] == undefined) {
+            //         continue
+            //     }
+            //     sidelicenter[j].children[2].style.borderColor = '#D2D2D2 transparent transparent';
+            //     // 兄弟元素
+            //     sidelicenter[j].nextElementSibling.style.display = 'none';
+            // }
+            // sideArr[i].pull = !sideArr[i].pull;
+            // if (sideArr[i].pull) {
+            //     sidelicenter[i].children[0].style.color = '#fff';
+            //     sidelicenter[i].children[1].style.color = '#fff';
+            //     if (sidelicenter[i].children[2] !== undefined) {
+            //         sidelicenter[i].children[2].style.borderColor = 'transparent transparent #fff';
+            //         sidelicenter[i].nextElementSibling.style.display = 'block';
+            //         sidedlpull();
+            //         sideShrink(side[0].offsetWidth);
+            //     }
+            // } else {
+            //     sidelicenter[i].children[0].style.color = '#D2D2D2';
+            //     sidelicenter[i].children[1].style.color = '#D2D2D2';
+            //     if (sidelicenter[i].children[2] !== undefined) {
+            //         sidelicenter[i].children[2].style.borderColor = '#fff transparent transparent';
+            //         sidelicenter[i].nextElementSibling.style.display = 'none';
+            //         sideShrink(side[0].offsetWidth);
+            //     }
+            // }
+
+            // console.log(side[0]);
+            // console.log(side[0].offsetWidth);
+            if (side[0].offsetWidth === 220) {
+                for (let j = 0; j < sidelicenter.length; j++) {
+                    if (i != j) {
+                        sideArr[j].pull = false;
+                    }
+                    sidelicenter[j].children[0].style.color = '#D2D2D2';
+                    sidelicenter[j].children[1].style.color = '#D2D2D2';
+                    if (sideli[j].children[0].children[2] == undefined) {
+                        continue
+                    }
+                    sidelicenter[j].children[2].style.borderColor = '#D2D2D2 transparent transparent';
+                    // 兄弟元素
+                    sidelicenter[j].nextElementSibling.style.display = 'none';
                 }
-                sideli[j].children[0].children[0].style.color = '#D2D2D2';
-                sideli[j].children[0].children[1].style.color = '#D2D2D2';
-                if (sideli[j].children[0].children[2] == undefined) {
-                    continue
-                }
-                sideli[j].children[0].children[2].style.borderColor = '#D2D2D2 transparent transparent';
-                sideli[j].children[1].style.display = 'none';
-            }
-            sideliArr[i].pull = !sideliArr[i].pull;
-            // sideflag = !sideflag;
-            // console.log(sideflag);
-            if (sideliArr[i].pull) {
-                sidelicolor.children[0].style.color = '#fff';
-                sidelicolor.children[1].style.color = '#fff';
-                if (sideli[i].children[0].children[2] !== undefined) {
-                    sideli[i].children[0].children[2].style.borderColor = 'transparent transparent #fff';
-                    sideli[i].children[1].style.display = 'block';
+                sideArr[i].pull = !sideArr[i].pull;
+                if (sideArr[i].pull) {
+                    sidelicenter[i].children[0].style.color = '#fff';
+                    sidelicenter[i].children[1].style.color = '#fff';
+                    if (sidelicenter[i].children[2] !== undefined) {
+                        sidelicenter[i].children[2].style.borderColor = 'transparent transparent #fff';
+                        sidelicenter[i].nextElementSibling.style.display = 'block';
+                        sidedlpull();
+                        // sideShrink(headerLeft[0].children[0].children[0].click());
+                    }
+                } else {
+                    sidelicenter[i].children[0].style.color = '#D2D2D2';
+                    sidelicenter[i].children[1].style.color = '#D2D2D2';
+                    if (sidelicenter[i].children[2] !== undefined) {
+                        sidelicenter[i].children[2].style.borderColor = '#fff transparent transparent';
+                        sidelicenter[i].nextElementSibling.style.display = 'none';
+                    }
                 }
             } else {
-                sidelicolor.children[0].style.color = '#D2D2D2';
-                sidelicolor.children[1].style.color = '#D2D2D2';
-                if (sideli[i].children[0].children[2] !== undefined) {
-                    sideli[i].children[0].children[2].style.borderColor = '#fff transparent transparent';
-                    sideli[i].children[1].style.display = 'none';
+                sideShrink(headerLeft[0].children[0].children[0].click());
+                for (let j = 0; j < sidelicenter.length; j++) {
+                    if (i != j) {
+                        sideArr[j].pull = false;
+                    }
+                    sidelicenter[j].children[0].style.color = '#D2D2D2';
+                    sidelicenter[j].children[1].style.color = '#D2D2D2';
+                    if (sideli[j].children[0].children[2] == undefined) {
+                        continue
+                    }
+                    sidelicenter[j].children[2].style.borderColor = '#D2D2D2 transparent transparent';
+                    // 兄弟元素
+                    sidelicenter[j].nextElementSibling.style.display = 'none';
+                }
+                sideArr[i].pull = !sideArr[i].pull;
+                if (sideArr[i].pull) {
+                    sidelicenter[i].children[0].style.color = '#fff';
+                    sidelicenter[i].children[1].style.color = '#fff';
+                    if (sidelicenter[i].children[2] !== undefined) {
+                        sidelicenter[i].children[2].style.borderColor = 'transparent transparent #fff';
+                        sidelicenter[i].nextElementSibling.style.display = 'block';
+                        sidedlpull();
+                    }
+                } else {
+                    sidelicenter[i].children[0].style.color = '#D2D2D2';
+                    sidelicenter[i].children[1].style.color = '#D2D2D2';
+                    if (sidelicenter[i].children[2] !== undefined) {
+                        sidelicenter[i].children[2].style.borderColor = '#fff transparent transparent';
+                        sidelicenter[i].nextElementSibling.style.display = 'none';
+                    }
                 }
             }
-            // console.log(sideliArr);
+
+            //     if (pull) {
+            //         for (let j = 0; j < sidelicenter.length; j++) {
+            //             if (i != j) {
+            //                 sideArr[j].pull = false;
+            //             }
+            //             sidelicenter[j].children[0].style.color = '#D2D2D2';
+            //             sidelicenter[j].children[1].style.color = '#D2D2D2';
+            //             if (sideli[j].children[0].children[2] == undefined) {
+            //                 continue
+            //             }
+            //             sidelicenter[j].children[2].style.borderColor = '#D2D2D2 transparent transparent';
+            //             // 兄弟元素
+            //             sidelicenter[j].nextElementSibling.style.display = 'none';
+            //         }
+            //         sideArr[i].pull = !sideArr[i].pull;
+            //         if (sideArr[i].pull) {
+            //             sidelicenter[i].children[0].style.color = '#fff';
+            //             sidelicenter[i].children[1].style.color = '#fff';
+            //             if (sidelicenter[i].children[2] !== undefined) {
+            //                 sidelicenter[i].children[2].style.borderColor = 'transparent transparent #fff';
+            //                 sidelicenter[i].nextElementSibling.style.display = 'block';
+            //                 sidedlpull();
+            //                 // sideShrink(headerLeft[0].children[0].children[0].click());
+            //             }
+            //         } else {
+            //             sidelicenter[i].children[0].style.color = '#D2D2D2';
+            //             sidelicenter[i].children[1].style.color = '#D2D2D2';
+            //             if (sidelicenter[i].children[2] !== undefined) {
+            //                 sidelicenter[i].children[2].style.borderColor = '#fff transparent transparent';
+            //                 sidelicenter[i].nextElementSibling.style.display = 'none';
+            //             }
+            //         }
+            //     } else {
+            //         for (let j = 0; j < sidelicenter.length; j++) {
+            //             if (i != j) {
+            //                 sideArr[j].pull = false;
+            //             }
+            //             sidelicenter[j].children[0].style.color = '#D2D2D2';
+            //             sidelicenter[j].children[1].style.color = '#D2D2D2';
+            //             if (sideli[j].children[0].children[2] == undefined) {
+            //                 continue
+            //             }
+            //             sidelicenter[j].children[2].style.borderColor = '#D2D2D2 transparent transparent';
+            //             // 兄弟元素
+            //             sidelicenter[j].nextElementSibling.style.display = 'none';
+            //         }
+            //         sideArr[i].pull = !sideArr[i].pull;
+            //         if (sideArr[i].pull) {
+            //             sidelicenter[i].children[0].style.color = '#fff';
+            //             sidelicenter[i].children[1].style.color = '#fff';
+            //             if (sidelicenter[i].children[2] !== undefined) {
+            //                 sidelicenter[i].children[2].style.borderColor = 'transparent transparent #fff';
+            //                 sidelicenter[i].nextElementSibling.style.display = 'block';
+            //                 sidedlpull();
+            //                 // sideShrink(headerLeft[0].children[0].children[0].click());
+            //             }
+            //         } else {
+            //             sidelicenter[i].children[0].style.color = '#D2D2D2';
+            //             sidelicenter[i].children[1].style.color = '#D2D2D2';
+            //             if (sidelicenter[i].children[2] !== undefined) {
+            //                 sidelicenter[i].children[2].style.borderColor = '#fff transparent transparent';
+            //                 sidelicenter[i].nextElementSibling.style.display = 'none';
+            //             }
+            //         }
+            // }
+
         }
     }
 }
-sideClick();
+sidelipull();
+
+
+sidedl[0].children[0].style.backgroundColor = '#808080';
+sidedl[0].children[0].children[0].style.color = '#fff';
+// console.log(sidedl[0].children);
+
+function sidedlpull() {
+    for (let i = 0; i < sidedl.length; i++) {
+        for (let j = 0; j < sidedl[i].children.length; j++) {
+            sidedl[i].children[j].onclick = function() {
+                for (let x = 0; x < sidedl[i].children.length; x++) {
+                    for (let y = 0; y < sidedl[i].children.length; y++) {
+                        sidedl[x].children[y].style.backgroundColor = '#3C3C3C';
+                        sidedl[x].children[y].children[0].style.color = '#D2D2D2';
+                    }
+                }
+                sidedl[i].children[j].style.backgroundColor = '#808080';
+                sidedl[i].children[j].children[0].style.color = '#fff';
+            }
+        }
+    }
+}
+
+var headerLeft = document.getElementsByClassName('header_left');
+// var side = document.getElementsByClassName('side');
+var sideTitle = document.getElementsByClassName('side_title');
+// console.log(headerLeft[0].children[0].children[0]);
+// console.log(side[0]);
+// sidelicenter[i].nextElementSibling.style.display = 'none';
+// console.log(sidelicenter.nextElementSibling);
+// getAttribute
+// console.log(sideArr);
+// console.log(sidedl);
+// console.log(sideTitle);
+
+
+function sideShrink(sideOpens) {
+    // sideOpen = !sideOpen;
+    // console.log(sideOpens);
+    headerLeft[0].children[0].children[0].onclick = function() {
+        // console.log(side[0].offsetWidth);
+        sideOpen = !sideOpen;
+        for (let i = 0; i < sidedl.length; i++) {
+            if (sideOpen) {
+                side[0].style.width = '220px';
+                for (let j = 0; j < sideArr.length; j++) {
+                    if (sideArr[j].pull && sidelicenter[j].children[2] !== undefined) {
+                        sidelicenter[j].nextElementSibling.style.display = 'block';
+                    }
+                }
+            } else {
+                side[0].style.width = '46px';
+                sidedl[i].style.display = 'none';
+            }
+        }
+    }
+}
+sideShrink();
