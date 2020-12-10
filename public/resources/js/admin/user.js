683d63bf942b5f3c4fb4ac3dd3b3ab3aec934f89
wdnmd <1459200389@qq.com>
2020-12-10 17:52:56 +0800

    切换页面bug修改完成

--- public/resources/js/admin/user.js
+++ public/resources/js/admin/user.js
@@ -3,15 +3,12 @@ var totals = new Number();
 $.ajax({
     type: 'get',
     url: '/admin/user',
-    // contentType: 'application/json',
     async: false,
     data: {
         // page: 5
     },
     success: function(data) {
-        // console.log('111');
         var { page, total } = data;
-        // console.log(page, total);
 
         // function pageButton() {
         var pageSize = ``;
@@ -30,11 +27,6 @@ $.ajax({
                 }
             }
         }
-        // document.getElementById('pg').innerHTML = `
-        //         <li><a href="javascript:;" class="userPageL" id="userPageL"><i class="iconfont icon-tubiaozhizuo-"></i></a></li>
-        //         ${pageSize}
-        //         <li><a href="javascript:;" class="userPageR" id="userPageR"><i class="iconfont icon-tubiaozhizuo-1"></i></a></li>
-        //         `;
         document.getElementById('pg').innerHTML = `${pageSize}`;
         pages = page;
         totals = total;
@@ -85,147 +77,14 @@ var pageFlag = false;
 
 function reqPage() {
     for (var i = 0; i < pageClick.length; i++) {
-        // pageClick[i].addEventListener('click',function(){
-        //     var thisPage = parseInt(this.children[0].innerHTML);
-        //     function pageFor(thatPage, page) {
-        //         if (page == 1) {
-        //             return `<li><a href="javascript:;">1</a></li>`;
-        //         } else if (thatPage - 2 > 1 && thatPage + 2 < page) {
-        //             return `
-        //                 <li><a href="javascript:;">1</a></li>
-        //                 <li id="pageEllipsis"><span>...</span></li>
-        //                 <li><a href="javascript:;">${thatPage - 1}</a></li>
-        //                 <li><a href="javascript:;">${thatPage}</a></li>
-        //                 <li><a href="javascript:;">${thatPage + 1}</a></li>
-        //                 <li id="pageEllipsis"><span>...</span></li>
-        //                 <li><a href="javascript:;">${page}</a></li>
-        //             `;
-        //         } else if (thatPage - 2 > 1 && thatPage + 2 >= page) {
-        //             if (thatPage + 2 == page) {
-        //                 return `
-        //                 <li><a href="javascript:;">1</a></li>
-        //                 <li id="pageEllipsis"><span>...</span></li>
-        //                 <li><a href="javascript:;">${thatPage - 1}</a></li>
-        //                 <li><a href="javascript:;">${thatPage}</a></li>
-        //                 <li><a href="javascript:;">${thatPage + 1}</a></li>
-        //                 <li><a href="javascript:;">${page}</a></li>
-        //             `;
-        //             } else if (thatPage === page) {
-        //                 return `
-        //                 <li><a href="javascript:;">1</a></li>
-        //                 <li id="pageEllipsis"><span>...</span></li>
-        //                 <li><a href="javascript:;">${page - 2}</a></li>
-        //                 <li><a href="javascript:;">${page - 1}</a></li>
-        //                 <li><a href="javascript:;">${page}</a></li>
-        //             `;
-        //             } else if (thatPage + 2 > page) {
-        //                 return `
-        //                 <li><a href="javascript:;">1</a></li>
-        //                 <li id="pageEllipsis"><span>...</span></li>
-        //                 <li><a href="javascript:;">${thatPage - 1}</a></li>
-        //                 <li><a href="javascript:;">${thatPage}</a></li>
-        //                 <li><a href="javascript:;">${page}</a></li>
-        //             `;
-        //             }
-        //         } else if (page <= 5) {
-        //             var userPageF = ``;
-        //             for (var i = 1; i <= page; i++) {
-        //                 userPageF += `<li><a href="javascript:;">${i}</a></li>`;
-        //             }
-        //             console.log(userPageF);
-        //             return userPageF;
-        //         } else if (page > 5 && thatPage <= 3) {
-        //             return `
-        //                 <li><a href="javascript:;">1</a></li>
-        //                 <li><a href="javascript:;">2</a></li>
-        //                 <li><a href="javascript:;">3</a></li>
-        //                 <li><a href="javascript:;">4</a></li>
-        //                 <li id="pageEllipsis"><span>...</span></li>
-        //                 <li><a href="javascript:;">${page}</a></li>
-        //             `;
-        //         }
-        //     }
-        //     if (pageFlag) {
-        //         $.ajax({
-        //             type: 'get',
-        //             url: '/admin/user',
-        //             async: false,
-        //             data: {
-        //                 thisPage: thisPage
-        //             },
-        //             success: function(data) {
-        //                 var { thatPage, page, total } = data;
-        //                 document.getElementById('pg').innerHTML = `${pageFor(thatPage, page)}`;
-        //                 pages = page;
-        //                 totals = total;
-        //                 reqPage();
-        //             },
-        //             error: function(err) {
-        //                 console.log(err);
-        //             }
-        //         });
-        //     }
-        //     pageFlag = true;
-        //     pageNum.value = this.children[0].innerHTML;
-        //     var thisIndex = parseInt(this.children[0].innerHTML);
-        //     if (thisIndex == 1) {
-        //         if (userPageR.classList.remove('aDisable')) {
-        //             userPageR.classList.remove('aDisable');
-        //         }
-        //         userPageL.classList.add('aDisable');
-        //     } else if (thisIndex == pages) {
-        //         if (userPageL.classList.remove('aDisable')) {
-        //             userPageL.classList.remove('aDisable');
-        //         }
-        //         userPageR.classList.add('aDisable');
-        //     } else if (pages == 1) {
-        //         userPageR.classList.add('aDisable');
-        //         userPageL.classList.add('aDisable');
-        //     } else {
-        //         userPageL.classList.remove('aDisable');
-        //         userPageR.classList.remove('aDisable');
-        //     }
-        //     for (var j = 0; j < pageClick.length; j++) {
-        //         if (pageClick[j].children[0].classList.remove('pageClick')) {
-        //             pageClick[j].children[0].classList.remove('pageClick');
-        //         }
-        //         pageClick[j].children[0].classList.add('rPageClick');
-        //     }
-        //     console.log(this.children[0]);
-        //     this.children[0].classList.remove('rPageClick');
-        //     this.children[0].classList.add('pageClick');
-        // });
+        pageClick[i].index = i;
         pageClick[i].onclick = function() {
-            console.log(typeof(this));
-
-            // console.log('333');
-            // console.log(this.index);
-            // switch (this.index) {
-            //     case 1:
-            //         userPageL.classList.add('aDisable');
-            //     case 5:
-            //         userPageR.classList.add('aDisable');
-            //     default:
-            //         if (userPageL.classList.remove('aDisable') && userPageR.classList.remove('aDisable')) {
-            //             userPageL.classList.remove('aDisable');
-            //             userPageR.classList.remove('aDisable');
-            //         }
-            // }
-            // console.log(this.index);
-            // console.log(pageClick.length - 2);
-            // console.log(this.children[0].innerHTML);
             var thisPage = parseInt(this.children[0].innerHTML);
-            // console.log(this.children[0].innerHTML);
-            // console.log(thisPage);
 
             function pageFor(thatPage, page) {
-                // console.log("pageFor");
-                // for(var i=0;i<;i++){}
                 if (page == 1) {
-                    // console.log('page == 1');
                     return `<li><a href="javascript:;">1</a></li>`;
                 } else if (thatPage - 2 > 1 && thatPage + 2 < page) {
-                    // console.log('thatPage - 2 > 1 && thatPage + 2 < page');
                     return `
                         <li><a href="javascript:;">1</a></li>
                         <li id="pageEllipsis"><span>...</span></li>
@@ -236,7 +95,6 @@ function reqPage() {
                         <li><a href="javascript:;">${page}</a></li>
                     `;
                 } else if (thatPage - 2 > 1 && thatPage + 2 >= page) {
-                    // console.log('thatPage - 2 > 1 && thatPage + 2 >= page');
                     if (thatPage + 2 == page) {
                         return `
                         <li><a href="javascript:;">1</a></li>
@@ -263,43 +121,13 @@ function reqPage() {
                         <li><a href="javascript:;">${page}</a></li>
                     `;
                     }
-                    // return `
-                    //     <li><a href="javascript:;">1</a></li>
-                    //     <li id="pageEllipsis"><span>...</span></li>
-                    //     <li><a href="javascript:;">${thatPage - 1}</a></li>
-                    //     <li><a href="javascript:;">${thatPage}</a></li>
-                    //     <li><a href="javascript:;">${thatPage + 1}</a></li>
-                    //     <li><a href="javascript:;">${page}</a></li>
-                    // `;
                 } else if (page <= 5) {
-                    // console.log('page <= 5');
                     var userPageF = ``;
                     for (var i = 1; i <= page; i++) {
                         userPageF += `<li><a href="javascript:;">${i}</a></li>`;
                     }
-                    console.log(userPageF);
                     return userPageF;
                 } else if (page > 5 && thatPage <= 3) {
-                    // console.log('page > 5 && thatPage <= 3');
-                    // if (thatPage == 1) {
-                    //     return `
-                    //         <li><a href="javascript:;">1</a></li>
-                    //         <li><a href="javascript:;">2</a></li>
-                    //         <li><a href="javascript:;">3</a></li>
-                    //         <li><a href="javascript:;">4</a></li>
-                    //         <li id="pageEllipsis"><span>...</span></li>
-                    //         <li><a href="javascript:;">${page}</a></li>
-                    //     `;
-                    // }else if(thatPage == 2){
-                    //     return `
-                    //         <li><a href="javascript:;">1</a></li>
-                    //         <li><a href="javascript:;">2</a></li>
-                    //         <li><a href="javascript:;">3</a></li>
-                    //         <li><a href="javascript:;">4</a></li>
-                    //         <li id="pageEllipsis"><span>...</span></li>
-                    //         <li><a href="javascript:;">${page}</a></li>
-                    //     `;
-                    // }
                     return `
                         <li><a href="javascript:;">1</a></li>
                         <li><a href="javascript:;">2</a></li>
@@ -320,43 +148,9 @@ function reqPage() {
                     },
                     success: function(data) {
                         var { thatPage, page, total } = data;
-                        // console.log(data);
-                        // console.log(thatPage, page, total);
-                        // document.getElementById('pg').innerHTML = `
-                        //         <li><a href="javascript:;" class="userPageL" id="userPageL"><i class="iconfont icon-tubiaozhizuo-"></i></a></li>
-                        //         ${pageFor(thatPage, page)}
-                        //         <li><a href="javascript:;" class="userPageR" id="userPageR"><i class="iconfont icon-tubiaozhizuo-1"></i></a></li>
-                        //         `;
                         document.getElementById('pg').innerHTML = `${pageFor(thatPage, page)}`;
-                        // console.log('加载完成');
                         pages = page;
                         totals = total;
-                        // if (page == 1) {
-                        //     document.getElementById('pg').innerHTML = `
-                        //         <li><a href="javascript:;" class="userPageL" id="userPageL"><i class="iconfont icon-tubiaozhizuo-"></i></a></li>
-                        //         <li><a href="javascript:;">1</a></li>
-                        //         <li><a href="javascript:;" class="userPageR" id="userPageR"><i class="iconfont icon-tubiaozhizuo-1"></i></a></li>
-                        //         `;
-                        // } else if (thatPage - 2 < 1) {
-                        //     document.getElementById('pg').innerHTML = `
-                        //         <li><a href="javascript:;" class="userPageL" id="userPageL"><i class="iconfont icon-tubiaozhizuo-"></i></a></li>
-                        //         ${pageFor()}
-                        //         <li><a href="javascript:;">1</a></li>
-                        //         <li><a href="javascript:;" class="userPageR" id="userPageR"><i class="iconfont icon-tubiaozhizuo-1"></i></a></li>
-                        //         `;
-                        // }
-                        // if (thatPage - 1 == 1) {
-                        //     document.getElementById('pg').innerHTML = `
-                        //         <li><a href="javascript:;" class="userPageL" id="userPageL"><i class="iconfont icon-tubiaozhizuo-"></i></a></li>
-                        //         <li><a href="javascript:;">1</a></li>
-                        //         <li><a href="javascript:;">2</a></li>
-                        //         <li><a href="javascript:;">3</a></li>
-                        //         <li id="pageEllipsis"><span>...</span></li>
-                        //         <li><a href="javascript:;">${page}</a></li>
-                        //         <li><a href="javascript:;" class="userPageR" id="userPageR"><i class="iconfont icon-tubiaozhizuo-1"></i></a></li>
-                        //         `;
-                        //     pageSize += `<li><a href="javascript:;">${i}</a></li>`;
-                        // } else if (thatPage - 1 == 1) {}
                         reqPage();
                     },
                     error: function(err) {
@@ -365,51 +159,42 @@ function reqPage() {
                 });
             }
             pageFlag = true;
-            // console.log(pageClick);
             pageNum.value = this.children[0].innerHTML;
             var thisIndex = parseInt(this.children[0].innerHTML);
             if (thisIndex == 1) {
-                // console.log('l');
                 if (userPageR.classList.remove('aDisable')) {
                     userPageR.classList.remove('aDisable');
                 }
                 userPageL.classList.add('aDisable');
             } else if (thisIndex == pages) {
-                // console.log('r');
                 if (userPageL.classList.remove('aDisable')) {
                     userPageL.classList.remove('aDisable');
                 }
                 userPageR.classList.add('aDisable');
             } else if (pages == 1) {
-                // console.log('lr');
                 userPageR.classList.add('aDisable');
                 userPageL.classList.add('aDisable');
             } else {
                 console.log('else');
-                // console.log('111');
                 userPageL.classList.remove('aDisable');
                 userPageR.classList.remove('aDisable');
-                // if (userPageL.classList.remove('aDisable') && userPageR.classList.remove('aDisable')) {
-                //     console.log('222');
-                //     userPageL.classList.remove('aDisable');
-                //     userPageR.classList.remove('aDisable');
-                // }
             }
             for (var j = 0; j < pageClick.length; j++) {
-                // console.log(pageClick[j].children[0]);
-                if (pageClick[j].children[0].classList.remove('pageClick')) {
-                    pageClick[j].children[0].classList.remove('pageClick');
+                pageClick[j].children[0].classList.add('rpageClick');
+                if (this.index == j) {
+                    pageClick[j].children[0].classList.remove('rpageClick');
+                    pageClick[j].children[0].classList.add('pageClick');
                 }
-                pageClick[j].children[0].classList.add('rPageClick');
             }
-            console.log(this.children[0]);
-            this.children[0].classList.remove('rPageClick');
-            this.children[0].classList.add('pageClick');
+            // this.children[0].classList.remove('rpageClick');
+            // this.children[0].classList.add('pageClick');
         }
     }
 }
 reqPage();
 pageClick[0].click();
+
+
 userPageL.onclick = function() {
     if (userPageL.classList.contains('aDisable')) {
         return;
@@ -459,13 +244,6 @@ function selectClick() {
     function stopFunc(e) {
         e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true;
     }
-    // document.onclick = function(e) {
-    //     console.log(111);
-    //     if (!roleFlag) { roleFlag = !roleFlag; }
-    //     selectRole.children[1].children[0].setAttribute('class', 'iconfont icon-xiasanjiaoxing');
-    //     roleUl.style.display = 'none';
-    // }
-    // console.log(roleUl.children);
     for (var i = 0; i < roleUl.children.length; i++) {
         roleUl.children[i].onclick = function() {
             roleText.innerHTML = this.innerHTML;
@@ -495,7 +273,6 @@ function selectClick() {
         e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true;
     }
     document.onclick = function(e) {
-        // console.log(222);
         if (!stateFlag) { stateFlag = !stateFlag; }
         selectState.children[1].children[0].setAttribute('class', 'iconfont icon-xiasanjiaoxing');
         stateUl.style.display = 'none';
